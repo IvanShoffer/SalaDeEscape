bb04541a7a98f8ef007af09e5a7752563fb8a092
IVAN DANIEL SHOFFER <48861728@ort>
2025-06-05 08:37:41 -0300

    Models

--- Models/Acertijo.cs
+++ Models/Acertijo.cs
@@ -1,29 +1,76 @@
 using Newtonsoft.Json;
 public class Acertijo
 {
+          [JsonProperty]
       public List<string> RespuestasCorrectas { get; set; } = new List<string>
         {
-            "Francia",         
-            "Lille",         
-            "Campos y Pico", 
+            "FRANCIA",         
+            "LILLE",         
+            "CAMPOS Y PICO", 
             "4312"         
         };
+        
+          [JsonProperty]
+      public List<string> pistas { get; set; } = new List<string>
+        {
+            "A simple vista, todo parece en orden... pero ¿y si uno de ellos estuviera mintiendo? Observá con atención los pasaportes. A veces, los errores revelan más de lo que ocultan.",         
+            "LILLE",         
+            "CAMPOS Y PICO", 
+            "4312"         
+        };
+                [JsonProperty]
 
- 
         public int SalaActual { get; set; }
+                [JsonProperty]
 
  
         public string RespuestaJugador { get; set; }
+                [JsonProperty]
 
   
         public bool gano { get; set; }
          public bool ValidarRespuesta(string RespuestaJugador)
   {
+
     RespuestaJugador = RespuestaJugador.ToUpper();
+       if (RespuestaJugador == RespuestasCorrectas[SalaActual])
+    {
+        gano = true;
+         
+        
+    }
+    else
+    {
+        gano = false;
+    }
+
+
+    return gano;
     
   }
-}
+           public bool ValidarRespuestaFinal(string RespuestaJugador)
+  {
+bool finalizo = false;
+    RespuestaJugador = RespuestaJugador.ToUpper();
+       if (RespuestaJugador == RespuestasCorrectas[3])
+    {
+        finalizo = true;
+         
+        
+    }
  
 
+
+    return finalizo;
+    
+  }
+  public string darPistas ()
+  {
+       string pista = pistas[SalaActual];
+       return pista;
+  }
+  
+}
+ 
 
 
