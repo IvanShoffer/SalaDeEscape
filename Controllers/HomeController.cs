f8f867bf9bc5f7f45f95cced4af1af65cba8fa2b
IVAN DANIEL SHOFFER <48861728@ort>
2025-06-06 15:01:42 -0300

    controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,10 +12,61 @@ public class HomeController : Controller
     {
         _logger = logger;
     }
+    
 
     public IActionResult Index()
     {
         return View();
     }
+    public IActionResult comoJugar()
+    {
+        return View("comoJugar");
+    }
+     public IActionResult participantes()
+    {
+        return View("participantes");
+    }
+     public IActionResult final()
+    {
+        return View("final");
+    }
+    public IActionResult InicializarAcertijo()
+    {
+       Acertijo acertijo = new Acertijo();
+       HttpContext.Session.SetString("juego", Objeto.ObjectToString(acertijo));
+
+
+
+        return View("Sala123");
+    }
     
+    public IActionResult Sala123(string respuesta)
+    {
+     Acertijo acertijo = Objeto.StringToObject<Acertijo>(HttpContext.Session.GetString("Juego"));
+     if(ViewBag.acertijo is null)
+
+    
+   
+    
+        if (!acertijo.ValidarRespuesta(respuesta))
+        {
+        
+            ViewBag.Mensaje = "Respuesta incorrecta. Intentá de nuevo.";
+        }
+
+        if(acertijo.SalaActual == 5){
+            return View("final");
+        }
+
+        
+    
+
+    ViewBag.pista = acertijo.darPistas();
+
+       HttpContext.Session.SetString("juego", Objeto.ObjectToString(acertijo));
+
+    return View("Sala"+acertijo.SalaActual);
+    }
 }
+  
+ 
--- Models/Acertijo.cs
+++ Models/Acertijo.cs
@@ -1,77 +1,79 @@
 using Newtonsoft.Json;
 public class Acertijo
 {
-          [JsonProperty]
-      public List<string> RespuestasCorrectas { get; set; } = new List<string>
+    [JsonProperty]
+    public List<string> RespuestasCorrectas { get; set; } = new List<string>
         {
-            "FRANCIA",         
-            "LILLE",         
-            "CAMPOS Y PICO", 
-            "4312"         
+            "FRANCIA",
+            "LILLE",
+            "CAMPOS Y PICO",
+            "4312"
         };
-        
-          [JsonProperty]
-      public List<string> pistas { get; set; } = new List<string>
+
+    [JsonProperty]
+    public List<string> pistas { get; set; } = new List<string>
         {
-            "A simple vista, todo parece en orden... pero ¿y si uno de ellos estuviera mintiendo? Observá con atención los pasaportes. A veces, los errores revelan más de lo que ocultan.",         
-            "Observá con atención lo primero de cada imagen, quizá ahí haya una clave",         
-            "Busca en el mapa los nombres que miran al cielo y tocan la tierra. La verdad está donde ambos se cruzan.", 
-            "4312"         
+            "A simple vista, todo parece en orden... pero ¿y si uno de ellos estuviera mintiendo? Observá con atención los pasaportes. A veces, los errores revelan más de lo que ocultan.",
+            "Observá con atención lo primero de cada imagen, quizá ahí haya una clave",
+            "Busca en el mapa los nombres que miran al cielo y tocan la tierra. La verdad está donde ambos se cruzan.",
+            "4312"
         };
-                [JsonProperty]
+    [JsonProperty]
 
-        public int SalaActual { get; set; }
-                [JsonProperty]
+    public int SalaActual { get; set; }
+    [JsonProperty]
 
- 
-        public string RespuestaJugador { get; set; }
-                [JsonProperty]
 
-  
-        public bool gano { get; set; }
+    public string RespuestaJugador { get; set; }
+    [JsonProperty]
 
-         public bool ValidarRespuesta(string RespuestaJugador)
-  {
 
-    RespuestaJugador = RespuestaJugador.ToUpper();
-       if (RespuestaJugador == RespuestasCorrectas[SalaActual])
+    public bool gano { get; set; }
+
+    public bool ValidarRespuesta(string RespuestaJugador)
     {
-        gano = true;
-         
-        
+
+        RespuestaJugador = RespuestaJugador.ToUpper();
+        if (RespuestaJugador == RespuestasCorrectas[SalaActual])
+        {
+            gano = true;
+            SalaActual++;
+
+
+        }
+        else
+        {
+            gano = false;
+        }
+
+
+        return gano;
+
     }
-    else
+    public bool ValidarRespuestaFinal(string RespuestaJugador)
     {
-        gano = false;
+        bool finalizo = false;
+        RespuestaJugador = RespuestaJugador.ToUpper();
+        if (RespuestaJugador == RespuestasCorrectas[3])
+        {
+            finalizo = true;
+
+
+        }
+
+
+
+        return finalizo;
+
     }
-
-
-    return gano;
-    
-  }
-           public bool ValidarRespuestaFinal(string RespuestaJugador)
-  {
- bool finalizo = false;
-    RespuestaJugador = RespuestaJugador.ToUpper();
-       if (RespuestaJugador == RespuestasCorrectas[3])
+    public string darPistas()
     {
-        finalizo = true;
-         
-        
+
+        string pista = pistas[SalaActual];
+        return pista;
     }
- 
-
-
-    return finalizo;
-    
-  }
-  public string darPistas ()
-  {
-       string pista = pistas[SalaActual];
-       return pista;
-  }
 
 }
- 
+
 
 
